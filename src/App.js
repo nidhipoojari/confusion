7e1549d997abb9d5f06a85a95cc3f393c899001d
Nidhi Poojari <nidhipoojari702@gmail.com>
2020-10-27 23:17:07 +0530

    card used

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/Menucomponent';
+import {DISHES} from './shared/dishes';
 
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
+ class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state= {
+      dishes: DISHES
+    }
+   }
+   
+   render () {
+    return (
+      <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes}/>
 
-    </div>
-  );
+  
+      </div>
+    );
+   }
+  
 }
 
 export default App;
